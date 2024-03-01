4ed7aa1532780ddb6631da06de149f42eda0ab7b
Bruna Santiago <brunasanthiago@outlook.com>
2024-03-01 19:47:25 -0300

    trocando por prisma

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 import express from 'express'
-import { env } from './env/index.js'
-import sequelize from './config/database.js'
+
+import { PrismaClient } from '@prisma/client'
 
 const app = express()
 app.use(express.json())
 
-sequelize.sync().then(() => {
-  app.listen(env.PORT, () => {
-    console.log('Server is running on port 3333')
-  })
-})
+const prisma = new PrismaClient()
+
+prisma.register.create({})
