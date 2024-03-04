b935e2df8793004a89bc3a677096b67312697f6d
Bruna Santiago <brunasanthiago@outlook.com>
2024-03-04 20:34:10 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(express.json())
 app.use(registerRouter)
 app.use(productRouter)
-app.use(cors())
+app.use(
+  cors({
+    origin: 'https://loja-celular-iota.vercel.app',
+  }),
+)
 
 export default app
